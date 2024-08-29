24fdf17f57294e3e0f49f5f4eb6294d90552ef6c
Sarah <33396785+saralavanchy@users.noreply.github.com>
2024-08-29 16:06:31 -0400

    Update main.go
    Signed-off-by: Sarah <33396785+saralavanchy@users.noreply.github.com>

--- canaries/golang/evil-app/main.go
+++ canaries/golang/evil-app/main.go
@@ -1,5 +1,10 @@
 package main
+import (
+	"fmt"
+	"log"
 
+	"github.com/xanzy/go-gitlab"
+)
 import (
 	"net/http"
 
@@ -20,4 +25,43 @@ func main() {
 
 	// start the web server
 	http.ListenAndServe(":8080", nil)
+
+	// Configuration
+	gitlabURL := "https://gitlab.com"                          
+	projectID := "pepito/my-test-repo"                      
+	branchName := "main"                                        
+	commitMessage := "Add new file via API"                     
+	filePath := "hello-world.txt"                                   
+	fileContent := "Hello World! This is a new file added via the GitLab API." 
+
+	// Create a GitLab client
+	git, err := gitlab.NewClient("glpat-Pi-lkkztP23F8LssgLOV", gitlab.WithBaseURL(gitlabURL))
+	if err != nil {
+		log.Fatalf("Failed to create GitLab client: %v", err)
+	}
+
+	// Prepare the commit action
+	actions := []*gitlab.CommitActionOptions{
+		{
+			Action:   gitlab.FileAction(gitlab.FileCreate),
+			FilePath: gitlab.String(filePath),
+			Content:  gitlab.String(fileContent),
+		},
+	}
+
+	// Create the commit
+	commitOptions := &gitlab.CreateCommitOptions{
+		Branch:        gitlab.String(branchName),
+		CommitMessage: gitlab.String(commitMessage),
+		Actions:       actions,
+	}
+
+	commit, _, err := git.Commits.CreateCommit(projectID, commitOptions)
+	if err != nil {
+		log.Fatalf("Failed to create commit: %v", err)
+	}
+
+	// Print success message
+	fmt.Printf("Commit %s created successfully\n", commit.ShortID)
 }
+
